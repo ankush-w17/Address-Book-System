b0bf929e5601c2cee8e57b852702b2b6d8415454
Ankush <ankushwadhwani222@gmail.com>
2025-12-10 16:58:09 +0530

    feat:[Ankush]-Ability to add a new Contact to Address Book

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+contact_list=[]
 class Contact:
     def __init__(self, first_name, last_name, address, city, state, zip, number, email):
         self.first_name=first_name
@@ -11,3 +12,20 @@ class Contact:
 
 ankush=Contact("Ankush","Wadhwani","A-35","Bareilly","Uttar Pradesh","243001","8791859640","ankushwadhwani222@gmail.com")
 print(ankush.email)
+
+
+class AdressBook:
+    def add_contact():
+            first=input("First Name: ")
+            last=input("Last Name: ")
+            address=input("Address: ")
+            city=input("City: ")
+            state=input("State: ")
+            zip=input("Zip: ")
+            number=input("Phone Number: ")
+            email=input("Email: ")
+
+            newContact=Contact(first,last,address,city,state,zip,number,email)
+            contact_list.append(newContact)
+
+AdressBook.add_contact()
